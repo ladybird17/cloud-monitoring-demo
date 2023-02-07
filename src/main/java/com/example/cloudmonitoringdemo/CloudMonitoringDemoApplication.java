8826b7dad311b5628b8fbdf99f605cca9976b1f3
yjseo <yjseo@iochord.com>
2023-02-07 10:47:15 +0900

    feat: Initialize project

---
+++ src/main/java/com/example/cloudmonitoringdemo/CloudMonitoringDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.cloudmonitoringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudMonitoringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudMonitoringDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cloudmonitoringdemo/CloudMonitoringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloudmonitoringdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudMonitoringDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
